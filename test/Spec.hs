9cfbe5c766441ef37ab3001690de2d2ca201b51b
jhamPac <cloud@kevia.me>
2021-11-04 10:32:52 -0700

    test: first round of tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import           Core
+import           RIO
+
+makeStep :: Text -> Text -> [Text] -> Step
+makeStep name image commands = Step {
+    name = StepName name,
+    image = Image image,
+    commands = commands
+}
+
+makePipeline :: [Step] -> Pipeline
+makePipeline steps = Pipeline { steps = steps }
+
+testPipeline :: Pipeline
+testPipeline = makePipeline
+    [
+        makeStep "Step 1" "ubuntu" ["date"],
+        makeStep "Step 2" "ubuntu" ["uname -r"]
+    ]
+
+testBuild :: Build
+testBuild = Build { pipeline = testPipeline, state = BuildReady}
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = pure ()
