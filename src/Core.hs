20fc4972b4798a1c1796961b794a5e9c443027b6
jhamPac <cloud@kevia.me>
2021-11-04 10:17:25 -0700

    feat: add type to build core

---
+++ src/Core.hs
@@ -0,0 +1,26 @@
+module Core where
+
+import           RIO
+
+data Pipeline = Pipeline { steps :: [Step] }
+    deriving (Eq, Show)
+
+data Step = Step {
+    name     :: StepName,
+    commands :: [Text],
+    image    :: Image
+} deriving (Eq, Show)
+
+data Build = Build { pipeline :: Pipeline } deriving (Eq, Show)
+
+newtype StepName = StepName Text
+    deriving (Eq, Show)
+
+newtype Image = Image Text
+    deriving (Eq, Show)
+
+stepNameToText :: StepName -> Text
+stepNameToText (StepName t) = t
+
+imageToText :: Image -> Text
+imageToText (Image t) = t
