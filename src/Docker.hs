ad079dba2fe15f572d87a8367ddc633ef59e7de7
jhamPac <cloud@kevia.me>
2021-11-07 11:55:19 -0700

    feat: build up a request

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,5 +1,6 @@
 module Docker where
 
+import qualified Network.HTTP.Simple as HTTP
 import           RIO
 
 data CreateContainerOptions = CreateContainerOptions {
@@ -13,10 +14,18 @@ newtype ContainerExitCode = ContainerExitCode Int
     deriving (Eq, Show)
 
 createContainer :: CreateContainerOptions -> IO ()
-createContainer options = undefined
+createContainer options = do
+    let body = ()
+    let req = HTTP.defaultRequest
+            & HTTP.setRequestPath "/containers/create"
+            & HTTP.setRequestMethod "POST"
+            & HTTP.setRequestBodyJSON body
+    res <- HTTP.httpBS req
+    traceShowIO res
 
 imageToText :: Image -> Text
 imageToText (Image t) = t
 
 exitCodeToInt :: ContainerExitCode -> Int
 exitCodeToInt (ContainerExitCode code) = code
+
