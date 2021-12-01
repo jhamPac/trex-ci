2b0044c720f11ccd3a457f5bf3804a0e416ed4cd
jhamPac <cloud@kevia.me>
2021-11-30 18:23:33 -0700

    feat: serialize post reponses and add instances

---
+++ src/Server.hs
@@ -0,0 +1,21 @@
+module Server where
+
+import qualified Codec.Serialise as Serialise
+import           Core
+import qualified JobHandler
+import           RIO
+import qualified Web.Scotty      as Scotty
+
+data Config = Config {
+        port :: Int
+    }
+
+run :: Config -> JobHandler.Service -> IO ()
+run config handler =
+    Scotty.scotty config.port do
+        Scotty.post "/agent/pull" do
+            cmd <- Scotty.liftAndCatchIO do
+                handler.dispatchCmd
+
+            Scotty.raw $ Serialise.serialise cmd
+        pure ()
