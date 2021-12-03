86555bab0be99f477d72d68cda078934c4dd9063
jhamPac <cloud@kevia.me>
2021-12-03 11:10:03 -0700

    feat: pass in configs for agents

--- src/Agent.hs
+++ src/Agent.hs
@@ -1,8 +1,11 @@
 module Agent where
 
-import qualified Codec.Serialise as Serialise
+import qualified Codec.Serialise     as Serialise
 import           Core
+import qualified Network.HTTP.Simple as HTTP
 import           RIO
+
+data Config = Config { endpoint :: String }
 
 data Cmd
     = StartBuild BuildNumber Pipeline
@@ -13,3 +16,14 @@ data Msg
     | BuildUpdated BuildNumber Build
     deriving (Eq, Show, Generic, Serialise.Serialise)
 
+run :: Config -> IO ()
+run config = forever do
+        endpoint <- HTTP.parseRequest config.endpoint
+
+        let req = endpoint
+                    & HTTP.setRequestMethod "POST"
+                    & HTTP.setRequestPath "/agent/pull"
+
+        res <- HTTP.httpLBS req
+        let cmd = Serialise.deserialise (HTTP.getResponseBody res) :: Maybe Cmd
+        threadDelay (1 * 1000 * 1000)
